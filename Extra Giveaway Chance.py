dec74d6e6e7497ab8e6d291f5019e39e83cbc694
Ideklo <77453748+Ideklo@users.noreply.github.com>
2021-01-14 16:48:30 +0000

    EGC EXE

---
+++ Extra Giveaway Chance.py
@@ -0,0 +1,20 @@
+#WHY YOU TRYANA BREAK IN ARE YOU DUM
+print ("Click On That Link To Get Started")
+print ("https://github.com/Ideklo/First-links")
+
+#
+while True:
+    password = input ("what is the password?")
+    if password == "hkajtian64315ngkan5jqnfkey":
+        print("ACCESS GRANTED")
+        print ("THANK YOU FOR BUYING EGC")
+        print ("TO CONTINUE CLICK ON THIS LINK https://github.com/Ideklo/EGC")
+        print ("once you done that you just follow what it says goodbye")
+        
+    else:
+        print("ACCESS DENIED")
+        #
+
+print ("THANK YOU FOR BUYING EGC")
+print ("TO CONTINUE CLICK ON THIS LINK file:///C:/Users/USER/Desktop/Just%20a%20bunch%20of%20code%20work%23/EGC.html")
+print ("once you done that you Just Follow What It Says Goodbye")
